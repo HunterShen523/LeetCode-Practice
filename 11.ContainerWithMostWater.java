66f676b780c5be4c190d3679bd4d3fe774551f2d
HunterShen523 <szh1105741299@gmail.com>
2022-08-04 20:15:16 -0400

    q11 upload

--- 11.ContainerWithMostWater.java
+++ 11.ContainerWithMostWater.java
@@ -1,23 +1,16 @@
-public class draft {
-
-    public static void main(String[] args) {
-    
-        int[] sample = new int[]{ 1,1 };
-        System.out.println(maxArea(sample));
-    }
-
-    public static int maxArea(int[] height) {
+class Solution {
+    public int maxArea(int[] height) {
         int tmp = 0;
         int max = 0;
         int maxheight = 0;
         int left = 0;
         int right = height.length-1;
-    
+
         // get max height
         for (int a : height){
             if (a>maxheight) maxheight=a;
         }
-    
+
         for (int i=1; i<=maxheight; i++) {
             while (height[left]<i) {
                 left++;
@@ -25,12 +18,9 @@ public class draft {
             while (height[right]<i) {
                 right--;
             }
-            
-
             if (left>right) break;
-        
+
             tmp = i*(right-left);
-            System.out.println(right + " "+left);
             if (tmp>max) max = tmp;
         }
         return max;
